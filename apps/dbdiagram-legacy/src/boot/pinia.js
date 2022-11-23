b366ba0912ad6c5e5b0983efad26633673202d09
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-23 07:48:27 +0300

    remove everything

--- apps/dbdiagram-legacy/src/boot/pinia.js
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia';
-import plugin from '../store/plugin';
-
-export default ({ app }) => {
-  const pinia = createPinia();
-  app.use(pinia);
-  pinia.use(plugin);
-};
--- apps/dbdiagram-legacy/src/components/ace/dbml.js
+++
@@ -1,5 +0,0 @@
-import { require } from 'ace-builds';
-import { ace } from 'ace-builds/src-noconflict/ace';
-
-
-define('')
