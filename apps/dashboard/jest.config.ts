0910bc0e9132e3a24a1620b09981d3588de2b069
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-24 07:13:53 +0300

    rename dashboard

--- apps/dashboard/jest.config.ts
+++ apps/dashboard/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'admin-dashboard',
+  displayName: 'dashboard',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/admin-dashboard',
+  coverageDirectory: '../../coverage/apps/dashboard',
 };
