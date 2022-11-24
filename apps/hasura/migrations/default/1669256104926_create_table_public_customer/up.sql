99bf02d14342a63e39d180527a52ccefd4f3c248
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-24 05:39:50 +0300

    updates

--- apps/hasura/migrations/default/1669256104926_create_table_public_customer/up.sql
+++ apps/hasura/migrations/default/1669256104926_create_table_public_customer/up.sql
@@ -1,2 +1,1 @@
 CREATE TABLE "public"."customer" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "app_id" uuid NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("app_id") REFERENCES "public"."app"("id") ON UPDATE restrict ON DELETE restrict, UNIQUE ("id"));
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
