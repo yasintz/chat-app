4fc4d2eabe88c7d386f79db676221327924348a5
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-25 00:18:55 +0300

    use old express app

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,22 +1,35 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+/* eslint-disable */
+// /**
+//  * This is not a production server yet!
+//  * This is only a minimal backend to get started.
+//  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+// import { Logger } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const globalPrefix = 'api';
+//   app.setGlobalPrefix(globalPrefix);
+//   const port = process.env.PORT || 3333;
+//   await app.listen(port);
+//   Logger.log(
+//     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+//   );
+// }
 
-bootstrap();
+// bootstrap();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.send('Hello world ' + Math.random());
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
