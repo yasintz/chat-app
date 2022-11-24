7533c8c1072fed9bb84b9cbb152de5fa8f3cd950
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-24 18:34:23 +0300

    updates

--- apps/hasura/scripts/docs/index.ts
+++ apps/hasura/scripts/docs/index.ts
@@ -5,7 +5,6 @@ import { environment } from '../environments/environment';
 export default function run() {
   build();
 
-  console.log({ environment });
   if (!environment.production) {
     chokidar
       .watch([
