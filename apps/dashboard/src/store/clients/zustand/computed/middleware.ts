49828879f921920947e180eaba7f8c60a6df0115
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-11-27 17:30:51 +0300

    typings

--- apps/dashboard/src/store/clients/zustand/computed/middleware.ts
+++ apps/dashboard/src/store/clients/zustand/computed/middleware.ts
@@ -1,63 +1,73 @@
-import { StateCreator } from 'zustand';
+import { StateCreator, StoreMutatorIdentifier } from 'zustand';
 import deepEqual from 'fast-deep-equal';
 import { ComputedFieldType, getComputedFieldKeys, isComputed } from './helpers';
 
-const middleware =
-  (f: StateCreator<any>): StateCreator<any> =>
-  (set, get, api) => {
-    type T = ReturnType<typeof f>;
+type ComputedMiddleware = <
+  T,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  f: StateCreator<T, Mps, Mcs>
+) => StateCreator<T, Mps, Mcs>;
 
-    const computeAndMerge = (state: T): T => {
-      const computedState: Record<string, any> = {};
-      const computedFields: Record<string, any> = {};
+type ComputedMiddlewareImpl = (
+  f: StateCreator<any, [], []>
+) => StateCreator<any, [], []>;
 
-      function compute(key: string, field: ComputedFieldType<any, any>) {
-        const { stateKey, fieldKey } = getComputedFieldKeys(key, field);
+const middleware: ComputedMiddlewareImpl = (f) => (set, get, api) => {
+  type T = ReturnType<typeof f>;
 
-        computedState[stateKey] = field(state);
+  const computeAndMerge = (state: T): T => {
+    const computedState: Record<string, any> = {};
+    const computedFields: Record<string, any> = {};
 
-        field.$$initialized = true;
-        computedFields[fieldKey] = field;
-      }
-      function batchCompute(field: ComputedFieldType<any, any>) {
-        field.$$initialized = true;
-        Object.assign(computedState, field(state));
-      }
+    function compute(key: string, field: ComputedFieldType<any, any>) {
+      const { stateKey, fieldKey } = getComputedFieldKeys(key, field);
 
-      Object.entries(state).forEach(([key, field]) => {
-        if (isComputed(field)) {
-          if (field.$$batch) {
-            batchCompute(field);
-          } else {
-            compute(key, field);
-          }
+      computedState[stateKey] = field(state);
+
+      field.$$initialized = true;
+      computedFields[fieldKey] = field;
+    }
+    function batchCompute(field: ComputedFieldType<any, any>) {
+      field.$$initialized = true;
+      Object.assign(computedState, field(state));
+    }
+
+    Object.entries(state).forEach(([key, field]) => {
+      if (isComputed(field)) {
+        if (field.$$batch) {
+          batchCompute(field);
+        } else {
+          compute(key, field);
         }
-      });
-
-      const newState = { ...state, ...computedState, ...computedFields };
-
-      for (const k of Object.keys(computedState)) {
-        if (!deepEqual(state[k], computedState[k])) {
-          newState[k] = computedState[k];
-        }
       }
+    });
 
-      return newState;
-    };
+    const newState = { ...state, ...computedState, ...computedFields };
 
-    const setWithComputed = (
-      update: T | ((state: T) => T),
-      replace?: boolean
-    ) => {
-      set((state: T): T => {
-        const updated = typeof update === 'object' ? update : update(state);
-        return computeAndMerge({ ...state, ...updated });
-      }, replace);
-    };
+    for (const k of Object.keys(computedState)) {
+      if (!deepEqual(state[k], computedState[k])) {
+        newState[k] = computedState[k];
+      }
+    }
 
-    api.setState = setWithComputed;
-    const st = f(setWithComputed, get, api);
-    return Object.assign({}, st, computeAndMerge(st));
+    return newState;
   };
 
-export default middleware;
+  const setWithComputed = (
+    update: T | ((state: T) => T),
+    replace?: boolean
+  ) => {
+    set((state: T): T => {
+      const updated = typeof update === 'object' ? update : update(state);
+      return computeAndMerge({ ...state, ...updated });
+    }, replace);
+  };
+
+  api.setState = setWithComputed;
+  const st = f(setWithComputed, get, api);
+  return Object.assign({}, st, computeAndMerge(st));
+};
+
+export default middleware as unknown as ComputedMiddleware;
