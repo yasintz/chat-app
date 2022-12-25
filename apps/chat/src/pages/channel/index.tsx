025a6c84b8c44d0e2cf7c41e455708a6074f1758
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-12-25 22:52:24 +0300

    [chat] use markdown lib for chat (#2)
    * use markdown lib for chat
    
    * add edited and createdTime
    
    * remove comment

--- apps/chat/src/pages/channel/index.tsx
+++ apps/chat/src/pages/channel/index.tsx
@@ -1,11 +1,15 @@
 //#region Import
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation, useSubscription } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { AuthenticatedPageLayout } from '../../components/common/layouts/AuthenticatedPageLayout';
 import { useAuthenticatedUserData } from '../../hooks/load-authenticated-user-data';
 import { gql } from '../../gql';
-import { MessageList } from '../../components/page/channel/message-list';
+
+import { ChatInput } from './chat-input';
+import { MessageItem } from './message-item';
 //#endregion
 
 //#region GQL
@@ -13,13 +17,7 @@ const getChannelMessages = gql(/* GraphQL */ `
   subscription getChannelMessages($channelId: uuid!) {
     message(where: { channelId: { _eq: $channelId } }) {
       id
-      body
-      parentId
-      replyToId
-      sender {
-        id
-        name
-      }
+      ...Message
     }
   }
 `);
@@ -33,22 +31,25 @@ const addNewMessage = gql(/* GraphQL */ `
     insert_message_one(
       object: { body: $body, channelId: $channelId, senderId: $senderId }
     ) {
-      body
-      parentId
-      replyToId
-      sender {
-        id
-        name
-      }
+      ...Message
     }
   }
 `);
 //#endregion
 
+// #region Styled
+const StyledMessageListContainer = styled.div`
+  overflow-y: scroll;
+  height: 600px;
+`;
+
+// #endregion
+
 export const ChannelPage = () => {
   const { channels, memberId, isLoading, error } = useAuthenticatedUserData();
   const { channelId } = useParams();
-  const [newMessage, setNewMessage] = React.useState<string>();
+  const [showPreview, setShowPreview] = useState(false);
+  const [newMessage, setNewMessage] = React.useState<string>('');
 
   const {
     data,
@@ -67,6 +68,10 @@ export const ChannelPage = () => {
     setNewMessage('');
   }, [createNewCustomer]);
 
+  const onPreviewClick = () => {
+    setShowPreview((prev) => !prev);
+  };
+
   if (isLoading || loading) {
     return <div>Loading...</div>;
   }
@@ -77,13 +82,26 @@ export const ChannelPage = () => {
 
   return (
     <AuthenticatedPageLayout channels={channels}>
-      <MessageList messageList={data?.message} />
+      <StyledMessageListContainer>
+        {data?.message.map((message) => (
+          <MessageItem key={message.id} message={message} />
+        ))}
+      </StyledMessageListContainer>
 
-      <input
+      <ChatInput
         value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
+        onChange={setNewMessage}
+        onPreview={onPreviewClick}
+        onSend={onMessageSent}
       />
-      <button onClick={onMessageSent}>Send Message</button>
+      {showPreview && (
+        <>
+          <b>Preview</b>
+          <hr />
+          <ReactMarkdown>{newMessage}</ReactMarkdown>
+          <hr />
+        </>
+      )}
     </AuthenticatedPageLayout>
   );
 };
